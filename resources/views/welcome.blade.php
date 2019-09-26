cba42b24441285a300ba11023cdac06f19b7e299
Balázs Kovács <balazs.kovacs@stylersonline.com>
2019-09-26 10:55:46 +0200

    Install Bootstrap & set up a basic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,20 @@
         <title>Laravel Training</title>
     </head>
     <body>
-        Laravel Training
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="/">Laravel Training</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/foo">Foo</a>
+                </li>
+            </ul>
+        </nav>
+
+        <main class="container">
+            <h1>Hello World</h1>
+        </main>
     </body>
 </html>
