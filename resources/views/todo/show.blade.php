71852e517d33ae4b3d5eb7a03a6b7676a0390532
Balázs Kovács <balazs.kovacs@stylersonline.com>
2019-09-30 13:21:25 +0200

    Delete my Todos

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -3,8 +3,16 @@
 @section('title', $todo->name)
 
 @section('content')
+
 <h1>{{ $todo->name }}</h1>
 <p>{{ $todo->description }}</p>
+
 <a href="{{ url("todo/{$todo->id}/edit") }}" class="btn btn-light">Edit</a>
+<form method="POST" action="{{ url('todo/' . $todo->id) }}" style="display: inline;">
+    @csrf
+    @method('DELETE')
+    <button type="submit" class="btn btn-danger">Delete</button>
+</form>
 <a href="{{ url('todo') }}" class="btn btn-primary">Back to My Todos</a>
+
 @endsection
